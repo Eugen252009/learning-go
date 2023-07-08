645bf5169b41a591437ece06f76cccc4e12b4379
Eugen <eugen252009@gmail.com>
2023-07-08 11:30:59 +0200

    added delete func

--- webserver/main.go
+++ webserver/main.go
@@ -55,5 +55,11 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+		}
+	}
 }
