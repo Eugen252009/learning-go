a574d8e5591f353b6a50fb7399a8fea5b9efa4b7
Eugen <eugen252009@gmail.com>
2023-07-08 22:02:41 +0200

    deleted revoled tokens

--- slackbot-fileuploader/main.go
+++ slackbot-fileuploader/main.go
@@ -8,8 +8,8 @@ import (
 )
 
 func main() {
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5545716986341-5561340133105-kWn39YRYPWDQtU8M2KTNWR3l")
-	os.Setenv("CHANNEL_ID", "C05G72N438U")
+	os.Setenv("SLACK_BOT_TOKEN", "")
+	os.Setenv("CHANNEL_ID", "")
 	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
 	channelArr := []string{os.Getenv("CHANNEL_ID")}
 	fileArr := []string{"pexels.jpg"}
