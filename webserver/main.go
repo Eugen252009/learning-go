6fa1027d23dd9f49ba2580aa5342011ebce1dd42
Eugen <eugen252009@gmail.com>
2023-07-08 11:35:16 +0200

    added GetMovie

--- webserver/main.go
+++ webserver/main.go
@@ -43,7 +43,14 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 }
 
 func getMovie(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
 }
 
 func createMovie(w http.ResponseWriter, r *http.Request) {
